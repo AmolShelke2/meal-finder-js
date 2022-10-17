dec59bfbfc88fcadfad140805a253526e19f65b4
Amol shelke <shelkeamol897@gmail.com>
2022-10-17 20:41:14 +0530

    Added common JS code

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Adding JS Logic
+
+const getMealButton = document.querySelector(".meal-button");
+const mealName = document.querySelector(".meal-name");
+const mealContainer = document.querySelector(".meal-container");
+const imageContainer = document.querySelector(".img-container");
+const description = document.querySelector(".description text");
