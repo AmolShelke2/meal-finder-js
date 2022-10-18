0a52866bc8335373d61712f9a652b1298089ca5a
Amol shelke <shelkeamol897@gmail.com>
2022-10-18 18:23:00 +0530

    fetching Meals

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ const mealName = document.querySelector(".meal-name");
 const mealContainer = document.querySelector(".meal-container");
 const imageContainer = document.querySelector(".img-container");
 const description = document.querySelector(".description text");
+
+// Fetching Meals
